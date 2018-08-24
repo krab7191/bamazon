d19983b7f252edaab699613f5043be7342d8ad9b
Karsten @ Ubuntu on Dell Latitude e6430s <karstenrabe91@gmail.com>
2018-08-24 00:23:37 -0400

    Flesh out schema.sql, make insert product statements

--- schema.sql
+++ schema.sql
@@ -1,15 +1,53 @@
+/* Delete the DB if it exists */
 drop database if exists bamazon;
 
+/* Re-create it */
 create DATABASE bamazon;
 
+/* use the newly created DB for future queries */
 use bamazon;
 
+/* Create the products table */
 create TABLE products
 (
     item_id INT NOT NULL AUTO_INCREMENT,
-    product_name VARCHAR (60) NOT NULL,
-    department_name VARCHAR(60) NOT NULL,
-    price INT NOT NULL,
-    stock_quantity INT,
+    product_name VARCHAR (100) NOT NULL,
+    department_name VARCHAR (100) NOT NULL,
+    price DECIMAL(10, 2) default 0,
+    stock_quantity INT default 0,
     PRIMARY KEY (item_id)
 );
+
+/* -- Populate with some items -- */
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Router", "networking", 34.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Bluetooth Adaptor", "entertainment", 12.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Smart Network Switch", "networking", 3.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("NAS Hard Drive", "PPE", 109.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Raspberry Pi", "DIY", 39.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Wifi Range Extender", "networking", 12.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("UPS", "electrical", 69.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Drone", "PPE", 128.56, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Cryptocurrency GPU rig", "PPE", 8000.00, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Spam", "food", 3.99, 100);
+
+insert into products (product_name, department_name, price, stock_quantity)
+values ("Eggs", "food", 1.29, 100);
